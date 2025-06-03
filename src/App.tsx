260389b20ccb8632111580421bd7d25d53ef6ac6
Angel Contreras <c3po.angel@gmail.com>
2025-06-02 20:19:06 -0600

    cv init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="max-w-screen-2xl mx-auto mt-10 p-5">
+        <section>
+          <div>
+            <h1>Sobre mi</h1>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
