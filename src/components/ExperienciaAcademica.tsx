b5c4760086a8cf4bb31772f933813e5a17159e05
Angel Contreras <c3po.angel@gmail.com>
2025-06-18 11:05:14 -0600

    translation

--- src/components/ExperienciaAcademica.tsx
+++ src/components/ExperienciaAcademica.tsx
@@ -1,21 +1,23 @@
+import { useTranslation } from "react-i18next";
 import Card from "./Card";
 
 export default function ExperienciaAcademica() {
+  const { t } = useTranslation("", { keyPrefix: "about-me.academic-exp" });
+
   return (
     <div className="space-y-1">
       <h2 className="font-bold text-2xl text-[#c7d2fe] py-3 lowercase first-letter:uppercase">
-        FORMACIÓN PROFESIONAL
+        {t("title")}
       </h2>
 
       <div className="grid  sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <Card>
           <div>
             <h2 className="text-2xl text-white font-semibold">
-              Ingenieria en desarrollo y gestion de software
+              {t("one.title")}
             </h2>
             <p className="font-light text-slate-200 text-sm">
-              Universidad Tecnologica de chihuahua,
-              <span className="italic">2021 -2023</span>
+              {t("one.school")},<span className="italic">2021 -2023</span>
             </p>
           </div>
         </Card>
@@ -23,21 +25,20 @@ export default function ExperienciaAcademica() {
         <Card>
           <div>
             <h2 className="text-2xl text-white font-semibold">
-              Licencia Profesional en Servicios móviles e interfaces nómada
+              {t("two.title")}
             </h2>
             <p className="font-light text-slate-200 text-sm">
-              IUT 1 Grenoble Francia ,<span className="italic">2020-2021</span>
+              {t("two.school")}, <span className="italic">2020-2021</span>
             </p>
           </div>
         </Card>
         <Card>
           <div>
             <h2 className="text-2xl text-white font-semibold">
-              TSU Desarrollo de software multiplataforma
+              {t("three.title")}
             </h2>
             <p className="font-light text-slate-200 text-sm">
-              Universidad Tecnologica de chihuahua{" "}
-              <span className="italic">2018-2020</span>
+              {t("three.school")} <span className="italic">2018-2020</span>
             </p>
           </div>
         </Card>
--- src/components/ExperienciaLaboral.tsx
+++ src/components/ExperienciaLaboral.tsx
@@ -1,4 +1,4 @@
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import Card from "./Card";
 
 export default function ExperienciaLaboral() {
@@ -13,13 +13,20 @@ export default function ExperienciaLaboral() {
           <>
             <div>
               <h2 className="text-2xl text-white font-semibold">
-                {t("work1")}
+                {t("work1.title")}
               </h2>
               <p className="font-light text-slate-200 text-sm">
                 Ganevales <span className="italic">03/2024 - 03/2025</span>
               </p>
             </div>
-            <p className="text-white">{t("work1-desc")}</p>
+            <p className="text-white">
+              <Trans i18nKey={"work1.desc"} t={t}>
+                Desarrollador de Frontend en{" "}
+                <span className="font-bold">React Native</span> para una
+                aplicación para digitalizar el proceso de negocio de la empresa
+                en todos sus puntos y departamentos
+              </Trans>
+            </p>
           </>
         </Card>
 
@@ -27,29 +34,19 @@ export default function ExperienciaLaboral() {
           <>
             <div>
               <h2 className="text-2xl text-white font-semibold">
-                {t("work2")}
+                {t("work2.title")}
               </h2>
               <p className="font-light text-slate-200 text-sm">
                 Ganevales <span className="italic">03/2024 - 03/2025</span>
               </p>
             </div>
-            <p className="text-white">{t("work2-desc")}</p>
-          </>
-        </Card>
-        <Card>
-          <>
-            <div>
-              <h2 className="text-2xl text-white font-semibold">
-                Desarrollador
-              </h2>
-              <p className="font-light text-slate-200 text-sm">
-                Hckr Labs <span className="italic">12/2021 - 10/2022</span>
-              </p>
-            </div>
             <p className="text-white">
-              Participé en el desarrollo aplicaciones y componentes frontend y
-              backend, contribuyendo en varios proyectos con la tecnologia de{" "}
-              <span className="font-bold">NextJs (react) y React native</span>.
+              <Trans i18nKey={"work2.desc"} t={t}>
+                Desarrollador en proyectos de software enfocados a los
+                departamentos de Ingenieria y Calidad para la digitalización de
+                procesos en el <span className="font-bold">SCADA Ignition</span>{" "}
+                y analisis de datos con <span className="font-bold">SQL</span>
+              </Trans>
             </p>
           </>
         </Card>
@@ -57,17 +54,39 @@ export default function ExperienciaLaboral() {
           <>
             <div>
               <h2 className="text-2xl text-white font-semibold">
-                Desarrollador en estadias
+                {t("work3.title")}
               </h2>
               <p className="font-light text-slate-200 text-sm">
-                Inria (Grenoble Francia){" "}
-                <span className="italic">2/2021 - 6/2021</span>
+                Hckr Labs <span className="italic">12/2021 - 10/2022</span>
               </p>
             </div>
             <p className="text-white">
-              Creación de interfaz web en <span className="bold">React</span> y
-              desarrollo de la integración del backend desarrollado en{" "}
-              <span className="font-bold">Python</span> para la interfaz web
+              <Trans i18nKey={"work3.desc"} t={t}>
+                Participé en el desarrollo aplicaciones y componentes frontend y
+                backend, contribuyendo en varios proyectos con la tecnologia de{" "}
+                <span className="font-bold">NextJs (react) y React native</span>
+                .
+              </Trans>
+            </p>
+          </>
+        </Card>
+        <Card>
+          <>
+            <div>
+              <h2 className="text-2xl text-white font-semibold">
+                {t("work4.title")}
+              </h2>
+              <p className="font-light text-slate-200 text-sm">
+                {t("work4.loc")} <span className="italic">2/2021 - 6/2021</span>
+              </p>
+            </div>
+            <p className="text-white">
+              <Trans i18nKey={"work4.desc"} t={t}>
+                Creación de interfaz web en{" "}
+                <span className="font-bold">React</span> y desarrollo de la
+                integración del backend desarrollado en{" "}
+                <span className="font-bold">Python</span> para la interfaz web
+              </Trans>
             </p>
           </>
         </Card>
